6fbd38a017b52206fb42a13c21390e870fe03e8b
cslack <cslack@sdl.com>
2017-12-22 21:01:55 -0500

    Added sonarqube info

--- src/main/python/netcontroller/view/mainwindow.py
+++ src/main/python/netcontroller/view/mainwindow.py
@@ -9,36 +9,36 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Ui_MainWindow(object):  #NOSONAR
-    def setupUi(self, MainWindow):  #NOSONAR
+class Ui_MainWindow(object):                                            # NOSONAR
+    def setupUi(self, MainWindow):                                      # NOSONAR
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(795, 467)
-        self.centralWidget = QtWidgets.QWidget(MainWindow)  #NOSONAR
+        self.centralWidget = QtWidgets.QWidget(MainWindow)              # NOSONAR
         self.centralWidget.setObjectName("centralWidget")
-        self.pushButton = QtWidgets.QPushButton(self.centralWidget)  #NOSONAR
+        self.pushButton = QtWidgets.QPushButton(self.centralWidget)     # NOSONAR
         self.pushButton.setGeometry(QtCore.QRect(470, 380, 113, 32))
         self.pushButton.setObjectName("pushButton")
-        self.tableWidget = QtWidgets.QTableWidget(self.centralWidget)  #NOSONAR
+        self.tableWidget = QtWidgets.QTableWidget(self.centralWidget)   # NOSONAR
         self.tableWidget.setGeometry(QtCore.QRect(100, 40, 551, 261))
         self.tableWidget.setObjectName("tableWidget")
         self.tableWidget.setColumnCount(0)
         self.tableWidget.setRowCount(0)
         MainWindow.setCentralWidget(self.centralWidget)
-        self.menuBar = QtWidgets.QMenuBar(MainWindow)  #NOSONAR
+        self.menuBar = QtWidgets.QMenuBar(MainWindow)                   # NOSONAR
         self.menuBar.setGeometry(QtCore.QRect(0, 0, 795, 22))
         self.menuBar.setObjectName("menuBar")
-        self.menuMyTest = QtWidgets.QMenu(self.menuBar)  #NOSONAR
+        self.menuMyTest = QtWidgets.QMenu(self.menuBar)                 # NOSONAR
         self.menuMyTest.setObjectName("menuMyTest")
-        self.menuAnother_Tab = QtWidgets.QMenu(self.menuBar)  #NOSONAR
+        self.menuAnother_Tab = QtWidgets.QMenu(self.menuBar)            # NOSONAR
         self.menuAnother_Tab.setObjectName("menuAnother_Tab")
         MainWindow.setMenuBar(self.menuBar)
-        self.mainToolBar = QtWidgets.QToolBar(MainWindow)  #NOSONAR
+        self.mainToolBar = QtWidgets.QToolBar(MainWindow)               # NOSONAR
         self.mainToolBar.setObjectName("mainToolBar")
         MainWindow.addToolBar(QtCore.Qt.TopToolBarArea, self.mainToolBar)
-        self.statusBar = QtWidgets.QStatusBar(MainWindow)  #NOSONAR
+        self.statusBar = QtWidgets.QStatusBar(MainWindow)               # NOSONAR
         self.statusBar.setObjectName("statusBar")
         MainWindow.setStatusBar(self.statusBar)
-        self.actionSomething = QtWidgets.QAction(MainWindow)  #NOSONAR
+        self.actionSomething = QtWidgets.QAction(MainWindow)            # NOSONAR
         self.actionSomething.setObjectName("actionSomething")
         self.menuMyTest.addAction(self.actionSomething)
         self.menuBar.addAction(self.menuMyTest.menuAction())
@@ -47,7 +47,7 @@ class Ui_MainWindow(object):  #NOSONAR
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-    def retranslateUi(self, MainWindow):  #NOSONAR
+    def retranslateUi(self, MainWindow):                                # NOSONAR
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.pushButton.setText(_translate("MainWindow", "Submit"))
